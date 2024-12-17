54ce10ae2e8b08a956473d2e08b41930da6cf093
Shukri <150862418+Shukrim433@users.noreply.github.com>
2024-12-17 14:54:34 +0000

    set up router in main.jsx and page components

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,60 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom/dist";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import App from "./App.jsx";
+import Error from "./pages/Error.jsx";
+import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
+import Signup from "./pages/Signup.jsx";
+import Doctors from "./pages/Doctors.jsx";
+import Doctor from "./pages/Doctor.jsx";
+import MyAppointments from "./pages/MyAppointments.jsx";
+import About from "./pages/About.jsx";
+import Contact from "./pages/Contact.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    error: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/signup",
+        element: <Signup />,
+      },
+      {
+        path: "/doctors",
+        element: <Doctors />,
+      },
+      {
+        path: "/doctor/:doctorId",
+        element: <Doctor />,
+      },
+      {
+        path: "/myAppointments",
+        element: <MyAppointments />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(
+  document.getElementById("root").render(<RouterProvider router={router} />)
+);
