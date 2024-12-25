da5b9feb335eea8740f4cc79ee6624b2af6e52e6
Shukri <150862418+Shukrim433@users.noreply.github.com>
2024-12-25 14:49:30 +0000

    re-seeded doctors to include the correct pathway to images.

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,12 +1,27 @@
 const db = require("../config/connection");
-const { Doctor } = require("../models");
+const { Doctor, Appointment } = require("../models");
 const doctorSeeds = require("./doctorSeeds.json");
+const appointmentSeeds = require("./appointmentSeeds.json")
 const cleanDB = require("./cleanDB");
 
 db.once("open", async () => {
   try {
+    await cleanDB("Appointment", "appointments");
     await cleanDB("Doctor", "doctors");
     await Doctor.create(doctorSeeds);
+
+    for (let i = 0; i < appointmentSeeds.length; i++) {
+      console.log("Creating appointment:", appointmentSeeds[i]);
+      const { _id, doctorId } = await Appointment.create(appointmentSeeds[i]);
+      const doctor =  await Doctor.findOneAndUpdate(
+        doctorId,
+        { $push: { appointments: _id } },
+        { new: true }
+      )
+      if(!doctor){
+        console.error(`no doctor with the id ${doctorId} found`)
+      }
+    }
   } catch (error) {
     console.log("error during seeding", error);
     process.exit(1);
