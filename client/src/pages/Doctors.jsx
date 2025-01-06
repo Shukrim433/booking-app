5b718c7fa72827eda38c050865e8434bec976bd7
Shukri <150862418+Shukrim433@users.noreply.github.com>
2025-01-06 15:07:03 +0000

    fixed logo in navbar and footer

--- client/src/pages/Doctors.jsx
+++ client/src/pages/Doctors.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import { useParams } from "react-router-dom";
 
 const Doctors = () => {
+  const { speciality } = useParams();
+  console.log("speciality", speciality);
   return (
-    <div>Doctors</div>
-  )
-}
+    <div>
+      Doctors
+      <p>hi</p>
+    </div>
+  );
+};
 
-export default Doctors
+export default Doctors;
