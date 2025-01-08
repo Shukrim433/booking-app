e91e6ca7f3692447b613c90599537241b2b603d2
Shukri <150862418+Shukrim433@users.noreply.github.com>
2025-01-08 15:36:50 +0000

    QUERY_DOCTOR query for front end

--- client/utils/queries.js
+++ client/utils/queries.js
@@ -25,10 +25,25 @@ export const QUERY_DOCTORS = gql`
   }
 `;
 
-// doctorsBySpeciality(speciality: String!): [Doctor]
+// doctorsBySpeciality(speciality: String): [Doctor]
 export const QUERY_DOCTORS_BY_SPECIALITY = gql`
   query getDoctorsBySpeciality($speciality: String) {
     doctorsBySpeciality(speciality: $speciality) {
+      _id
+      name
+      image
+      speciality
+      degree
+      experience
+      available
+    }
+  }
+`;
+
+// doctor(_id: ID!): Doctor
+export const QUERY_DOCTOR = gql`
+  query getDoctor($_id: ID!) {
+    doctor(_id: $_id) {
       _id
       name
       email
@@ -39,6 +54,14 @@ export const QUERY_DOCTORS_BY_SPECIALITY = gql`
       about
       available
       fees
+      address_line_1
+      address_line_2
+      appointments {
+        _id
+        slot_date
+        slot_month
+        slot_time
+      }
     }
   }
 `;
