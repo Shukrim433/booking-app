1c62309a7258032c0306e180fbcac971b1f8c881
Shukri <150862418+Shukrim433@users.noreply.github.com>
2025-09-03 14:36:05 +0100

    created login mutation

---
+++ client/utils/mutations.js
@@ -0,0 +1,14 @@
+import { gql } from "@apollo/client";
+
+// login(email: String!, password: String!): Auth
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
