237d4b8047a48cb0f5092b8adc17ef234293fbe5
Shukri <150862418+Shukrim433@users.noreply.github.com>
2025-10-01 11:01:40 +0100

    completed functionality for login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,22 +1,22 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useMutation } from "@apollo/client";
-import { LOGIN_USER } from "../../utils/mutations";
-import Auth from "../../utils/auth"
-import toast from "react-hot-toast"
+import { LOGIN_USER } from "../utils/mutations";
+import Auth from "../utils/auth";
+import toast from "react-hot-toast";
 
 const Login = () => {
   const navigate = useNavigate();
+  const [login, { error }] = useMutation(LOGIN_USER);
+
   const [formState, setFormState] = useState({
     email: "",
     password: "",
   });
 
-  const [login, { error }] = useMutation(LOGIN_USER);
-
   useEffect(() => {
     if (Auth.loggedIn()) {
-      navigate("/me");
+      navigate("/");
     }
   }, [navigate]);
 
@@ -47,39 +47,46 @@ const Login = () => {
   return (
     <div className="flex items-center justify-center  text-slate-800 py-8">
       {/* CONTAINER */}
-      <div className="flex flex-col border rounded-lg shadow-md px-10 py-8">
-        <p className="font-medium text-2xl">Login</p>
-        <p>Please login to book appointment</p>
+      <div className="flex flex-col border rounded-lg w-2/3 shadow-md px-10 py-10">
+        <p className="font-medium text-2xl mb-1">Login</p>
+        <p className="mb-3">Please login to book appointment</p>
 
         {/* FORM */}
         <form
           onSubmit={handleFormSubmit}
-          className="flex flex-col justify-center mt-6 gap-2"
+          className="flex flex-col justify-center gap-3"
         >
-          <p>Email</p>
-          <input
-            className="border rounded-md"
-            type="email"
-            name="email"
-            value={formState.email}
-            onChange={handleChange}
-          />
-          <p>Password</p>
-          <input
-            className="border rounded-md"
-            type="password"
-            name="password"
-            value={formState.password}
-            onChange={handleChange}
-          />
+          {/* Email */}
+          <div className="flex flex-col">
+            <p>Email:</p>
+            <input
+              className="border rounded-md pl-3 py-1"
+              type="email"
+              name="email"
+              value={formState.email}
+              onChange={handleChange}
+            />
+          </div>
+          {/* Password */}
+          <div className="flex flex-col">
+            <p>Password:</p>
+            <input
+              className="border rounded-md pl-3 py-1"
+              type="password"
+              name="password"
+              value={formState.password}
+              onChange={handleChange}
+            />
+          </div>
           <div>
             <button
               type="submit"
-              className="mt-6 mb-6 w-full bg-primary py-1 text-white rounded-md"
+              className="rounded-md py-1.5 w-full bg-primary text-white my-3"
             >
               {" "}
               Login{" "}
             </button>
+            {error && <div className="text-red-600 mt-2">{error.message}</div>}
           </div>
         </form>
 
